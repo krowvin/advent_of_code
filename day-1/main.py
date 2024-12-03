b5bf89f9d31069a75ee2b351bcb213b69e130baf
Charles Graham, SWT <charles.r.graham@usace.army.mil>
2024-12-02 23:10:27 -0600

    Day 1 - Part 1 - Answer: 27647262

--- day-1/main.py
+++ day-1/main.py
@@ -9,21 +9,40 @@ def readInput(input_file_path: str):
     # Break each column into its own array
     for line in total_lines:
         left, right = line.split(",")
-        left_list.append(left)
-        right_list.append(right)
+        left_list.append(int(left))
+        right_list.append(int(right))
 
     # Sort each column
     left_list.sort()
     right_list.sort()
+    return left_list, right_list
 
+
+def calc_total_distance(left_list, right_list):
+    """Determines what is the total distance between left and right column sorted"""
     total_distance = 0
-    for idx, line in enumerate(total_lines):
-        total_distance += abs(int(left_list[idx]) - int(right_list[idx]))
+    for idx, _ in enumerate(left_list):
+        total_distance += abs(left_list[idx] - right_list[idx])
         print(
             f"{idx}\t:\t[left: {left_list[idx]}] [right: {right_list[idx]}]\t-\t{abs(int(left_list[idx]) - int(right_list[idx]))}\t-\t{total_distance}"
         )
     print(f"Total Distance: {total_distance}")
 
 
+def calc_sim_score(left_list, right_list):
+    """Calculate a total similarity score by adding up each number
+    in the left list after multiplying it by the number of times
+    that number appears in the right list.
+    """
+    similarity_score = 0
+    for l_row_item in left_list:
+        # Multiply the row item on the left column
+        #   by the # times it appears on the right column
+        similarity_score += l_row_item * right_list.count(l_row_item)
+    print(f"Similarity Score: {similarity_score}")
+
+
 if __name__ == "__main__":
-    readInput("./input.txt")
+    left_list, right_list = readInput("./input.txt")
+    # calc_total_distance(left_list, right_list)
+    calc_sim_score(left_list, right_list)
